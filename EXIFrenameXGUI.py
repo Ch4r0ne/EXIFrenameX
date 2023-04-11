f987043090601a05981656ff8a9ba3391a290466
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2023-04-11 23:28:59 +0200

    Update EXIFrenameXGUI.py

--- EXIFrenameXGUI.py
+++ EXIFrenameXGUI.py
@@ -2,9 +2,22 @@ import os
 import exifread
 import pymediainfo
 import datetime
-from tqdm import tqdm
-import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
+from tkinter import *
+from tqdm import tqdm
+
+class TqdmOutput:
+    def write(self, s):
+        if s.rstrip() != "":
+            output.config(state=NORMAL)
+            output.insert(INSERT, s)
+            output.see(END)
+            output.config(state=DISABLED)
+            root.update_idletasks()
+
+    def flush(self):
+        pass
 
 # function to extract date from exif tags of .jpeg, .jpg, .png, ... files
 def get_exif_date(file_path):
@@ -37,16 +50,18 @@ def get_media_date(file_path):
             else:
                 return None
 
-def process_files():
-    folder_path = folder_entry.get()
-    renamed_files = []
-    files_without_metadata = []
+def start_script():
+    output.config(state=NORMAL)
+    output.delete(1.0, END)
 
-    # iterate over all files in the folder
+    folder = folder_path.get()
+    output.insert(INSERT, f"Verarbeite Dateien im Ordner: {folder}\n")
+
     renamed_files = []
     files_without_metadata = []
-    for filename in tqdm(os.listdir(folder_path)):
-        file_path = os.path.join(folder_path, filename)
+    
+    for filename in tqdm(os.listdir(folder), desc="Dateien umbenennen", unit="file", file=TqdmOutput()):
+        file_path = os.path.join(folder, filename)
         if os.path.isfile(file_path):
             extension = os.path.splitext(file_path)[1].lower()
             if extension in ['.jpg', '.jpeg', '.png', '.arw', '.nef', '.tiff', '.webp', '.bmp', '.cr2', '.orf', '.rw2', '.rwl', '.srw']:
@@ -57,7 +72,7 @@ def process_files():
                 datetime_obj = None
 
             if datetime_obj is not None:
-                new_name = datetime_obj.strftime("%Y-%m-%d_%H-%M-%S") + extension
+                new_name = datetime_obj.strftime(selected_format.get()) + extension
                 # check if new name already exists, if yes, add an incremental number
                 if new_name in renamed_files:
                     i = 1
@@ -65,45 +80,52 @@ def process_files():
                         new_name = datetime_obj.strftime("%Y-%m-%d_%H-%M-%S") + '_' + str(i) + extension
                         i += 1
                 try:
-                    os.rename(file_path, os.path.join(folder_path, new_name))
+                    os.rename(file_path, os.path.join(folder_path.get(), new_name))
                     renamed_files.append(new_name)
                 except FileExistsError:
                     i += 1
                     new_name = datetime_obj.strftime("%Y-%m-%d_%H-%M-%S") + '_' + str(i) + extension
-                    os.rename(file_path, os.path.join(folder_path, new_name))
+                    os.rename(file_path, os.path.join(folder_path.get(), new_name))
                     renamed_files.append(new_name)
             else:
                 files_without_metadata.append(filename)
 
-    # Update the status label
-    status_label.config(text="All files with metadata were successfully renamed.")
-    if files_without_metadata:
-        status_label.config(text="Some files could not be renamed due to missing metadata.")
+def browse_folder():
+    global folder_path
+    folder_path.set(filedialog.askdirectory())
 
-def open_folder():
-    folder_path = filedialog.askdirectory()
-    folder_entry.delete(0, tk.END)
-    folder_entry.insert(0, folder_path)
+root = Tk()
+root.title("EXIFrenameX")
+root.geometry("800x400")
+root.configure(bg='#D3D3D3')
 
-# Create the main window
-root = tk.Tk()
-root.title("File Renamer")
+folder_path = StringVar()
+selected_format = StringVar()
 
-# Create the layout
-folder_label = tk.Label(root, text="Folder Path:")
-folder_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+left_frame = Frame(root, bg='#D3D3D3')
+left_frame.pack(side=LEFT, fill=BOTH, expand=1)
 
-folder_entry = tk.Entry(root, width=50)
-folder_entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+# Change text colours and background colours accordingly
+Label(left_frame, text="Ordner auswählen:", bg='#D3D3D3', fg='#000000').pack(anchor=NW, padx=5, pady=5)
+Entry(left_frame, textvariable=folder_path).pack(fill=X, padx=5, pady=5)
+Button(left_frame, text="Durchsuchen", command=browse_folder, bg='#A9A9A9', fg='#000000').pack(fill=X, padx=5, pady=5)
 
-browse_button = tk.Button(root, text="Browse", command=open_folder)
-browse_button.grid(row=0, column=2, padx=10, pady=10)
+Label(left_frame, text="Format auswählen:", bg='#D3D3D3', fg='#000000').pack(anchor=NW, padx=5, pady=5)
+format_options = ["%Y-%m-%d_%H-%M-%S", "%Y%m%d_%H%M%S", "%d-%m-%Y_%Hh%Mm%Ss"]
+dropdown = ttk.Combobox(left_frame, values=format_options, textvariable=selected_format, state="normal")
+dropdown.pack(fill=X, padx=5, pady=5)
 
-rename_button = tk.Button(root, text="Rename Files", command=process_files)
-rename_button.grid(row=1, column=0, columnspan=3, pady=10)
+Button(left_frame, text="Skript starten", command=start_script, bg='#A9A9A9', fg='#000000').pack(fill=X, padx=5, pady=5)
 
-status_label = tk.Label(root, text="")
-status_label.grid(row=2, column=0, columnspan=3, pady=10)
+right_frame = Frame(root, bg='#D3D3D3')
+right_frame.pack(side=RIGHT, fill=BOTH, expand=1)
 
-# Start the main GUI loop
+Label(right_frame, text="CMD-Ausgabe:", bg='#D3D3D3', fg='#000000').pack(anchor=NW, padx=5, pady=5)
+
+output = Text(right_frame, wrap=WORD, state=DISABLED, bg='#A9A9A9', fg='#000000')
+output.pack(fill=BOTH, expand=1, padx=5, pady=5)
+scrollbar = Scrollbar(right_frame, command=output.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
+output.config(yscrollcommand=scrollbar.set)
+
 root.mainloop()
