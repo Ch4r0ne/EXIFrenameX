c11a4873a80613aaa11ea6a7dc2baf3430870a5c
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2023-04-16 01:09:08 +0200

    Update EXIFrenameXGUI.py

--- EXIFrenameXGUI.py
+++ EXIFrenameXGUI.py
@@ -6,9 +6,6 @@ import tkinter
 import tkinter.filedialog
 import tkinter.messagebox
 import customtkinter
-import tkinter.messagebox as messagebox
-from tqdm import tqdm
-from io import StringIO
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -21,12 +18,12 @@ class App(customtkinter.CTk):
         self.title("EXIFrenameX")
         self.geometry(f"{1100}x{580}")
 
-        # configure grid layout (4x4)
+        # configure grid layout
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
+        # create GUI
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
@@ -35,47 +32,32 @@ class App(customtkinter.CTk):
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_1.configure(text="Rename Files", command=self.on_sidebar_button_1_click)
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
-                                                                       command=self.change_appearance_mode_event)
+        #self.sidebar_button_1.bind("<ButtonRelease-1>", lambda _: self.update_preview())
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"], command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
-
-        # create main entry and button
         self.entry = customtkinter.CTkEntry(self, placeholder_text="Brows File Pfad")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
-
         self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Browse", command=self.browse_directory)
         self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-
-        # create textbox_1 for Format Info
         self.textbox_1 = customtkinter.CTkTextbox(self, width=250)
         self.textbox_1.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
-        # create frame for format selection
         self.format_frame = customtkinter.CTkFrame(self, width=250)
         self.format_frame.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
         self.select_format_label = customtkinter.CTkLabel(self.format_frame, text="Select Format")
         self.select_format_label.grid(row=0, column=0, padx=20, pady=(10, 10), sticky="w")
-
         self.combobox_1 = customtkinter.CTkComboBox(self.format_frame, values=["%Y-%m-%d_%H-%M-%S", "%Y%m%d_%H%M%S", "%d-%m-%Y_%Hh%Mm%Ss"], width=240)
         self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
         #self.combobox_1.bind("<<ComboboxSelected>>", lambda _: self.update_preview())
-
         self.entry_2 = customtkinter.CTkEntry(self.format_frame, width=240, placeholder_text="Enter Prefix here")
         self.entry_2.grid(row=2, column=0, padx=20, pady=(10, 10))
         self.entry_2.bind("<KeyRelease>", lambda _: self.update_preview())
-
         self.entry_3 = customtkinter.CTkEntry(self.format_frame, width=240, placeholder_text="Enter Suffix here")
         self.entry_3.grid(row=3, column=0, padx=20, pady=(10, 10))
         self.entry_3.bind("<KeyRelease>", lambda _: self.update_preview())
-
         self.update_button = customtkinter.CTkButton(self.format_frame, text="Update Preview", command=self.update_preview)
         self.update_button.grid(row=4, column=0, padx=20, pady=(10, 10), sticky="nsew")
-
-        # create radiobutton frame
         self.radiobutton_frame = customtkinter.CTkFrame(self)
         self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
         self.radio_var = tkinter.IntVar(value=0)
@@ -90,12 +72,8 @@ class App(customtkinter.CTk):
         self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="w")
         self.radio_button_4 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=3)
         self.radio_button_4.grid(row=4, column=2, pady=10, padx=20, sticky="w")
-
-        # create textbox_2 and progressbar frame
         self.textbox_2 = customtkinter.CTkTextbox(self, width=250)
         self.textbox_2.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
-        # create textbox_3 File Preview
         self.textbox_3 = customtkinter.CTkTextbox(self, width=250)
         self.textbox_3.grid(row=1, column=2, columnspan=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
 
@@ -144,81 +122,87 @@ class App(customtkinter.CTk):
                     return None
                            
     def on_sidebar_button_1_click(self):
-        self.textbox_2.delete("0.0", tkinter.END)
         folder_path = self.entry.get()
         if not os.path.exists(folder_path):
+            self.textbox_2.delete("0.0", tkinter.END)
             self.textbox_2.insert("0.0", "Please select a folder path\n\n")
             return
-
+        progress_updater = ProgressUpdater(self, 0)
         renamed_files = []
         files_without_metadata = []
-        total_files = len(os.listdir(folder_path))
-        self.textbox_2.insert("0.0", f"Processing {total_files} files...\n\n")
-
-        progress_updater = ProgressUpdater(self, total_files, prefix="Progress:", suffix="Complete", length=15)
-        for i, filename in enumerate(os.listdir(folder_path), start=1):
-            progress_updater.update(i)
+        progress_updater.total = len(os.listdir(folder_path))
+        for index, filename in enumerate(os.listdir(folder_path)):
             file_path = os.path.join(folder_path, filename)
             if os.path.isfile(file_path):
                 extension = os.path.splitext(file_path)[1].lower()
-
                 if extension in ['.jpg', '.jpeg', '.png', '.arw', '.nef', '.tiff', '.webp', '.bmp', '.cr2', '.orf', '.rw2', '.rwl', '.srw']:
                     datetime_obj = self.get_exif_date(file_path)
                 elif extension in ['.mov', '.mp4']:
                     datetime_obj = self.get_media_date(file_path)
                 else:
                     datetime_obj = None
-
                 if datetime_obj is not None:
-                    new_name = self.get_formatted_date(datetime_obj, filename) + extension
-                    new_file_path = os.path.join(folder_path, new_name)
+                    new_name = self.get_formatted_date(datetime_obj, filename, index=index) + extension
+                    if new_name in renamed_files:
+                        i = 1
+                        while new_name in renamed_files:
+                            new_name = self.get_formatted_date(datetime_obj, filename, index=index) + '_' + str(i) + extension
+                            i += 1
                     try:
-                        os.rename(file_path, new_file_path)
+                        os.rename(file_path, os.path.join(folder_path, new_name))
                         renamed_files.append(new_name)
                     except FileExistsError:
-                        i = 1
-                        while os.path.exists(new_file_path):
-                            new_name = self.get_formatted_date(datetime_obj, filename, index=i) + extension
-                            new_file_path = os.path.join(folder_path, new_name)
-                            i += 1
-                        os.rename(file_path, new_file_path)
+                        i += 1
+                        new_name = self.get_formatted_date(datetime_obj, filename, index=index) + '_' + str(i) + extension
+                        os.rename(file_path, os.path.join(folder_path, new_name))
                         renamed_files.append(new_name)
                 else:
                     files_without_metadata.append(filename)
 
-        self.textbox_2.insert("0.0", "Processing complete.\n\n")
-        if files_without_metadata:
-            self.textbox_2.insert("0.0", "Files without metadata:\n" + "\n".join(files_without_metadata) + "\n\n")
+            progress_updater.update(index+1)
+
+        # update the preview in textbox_2
+        self.textbox_2.delete("0.0", tkinter.END)
+        self.textbox_2.insert("0.0", f"Files renamed successfully:")
+        for file in renamed_files:
+            self.textbox_2.insert("end", f"")
+        if len(files_without_metadata) > 0:
+            self.textbox_2.insert("end", f"\n\nThe following files have no metadata and were not renamed:\n")
+            for file in files_without_metadata:
+                self.textbox_2.insert("end", f"{file}\n")
+        self.textbox_3.see("end")
 
     def update_preview(self):
-        folder_path = self.entry.get()
-        if not os.path.exists(folder_path):
-            self.textbox_3.delete("0.0", tkinter.END)
-            self.textbox_3.insert("0.0", "Please select a folder path\n\n")
-            return
+        try:
+            folder_path = app.entry.get()
+            if not os.path.exists(folder_path):
+                app.textbox_3.delete("0.0", tkinter.END)
+                app.textbox_3.insert("0.0", "Please select a folder path\n\n")
+                return
 
-        self.textbox_3.delete("0.0", tkinter.END)
-        self.textbox_3.insert("0.0", "Preview of Files (0-49):\n\n")
+            app.textbox_3.delete("0.0", tkinter.END)
+            app.textbox_3.insert("0.0", "Preview of Files (0-49):\n\n")
 
-        file_count = 0
-        for filename in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, filename)
-            if os.path.isfile(file_path):
-                extension = os.path.splitext(file_path)[1].lower()
+            file_count = 0
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
+                if os.path.isfile(file_path):
+                    extension = os.path.splitext(file_path)[1].lower()
 
-                if extension in ['.jpg', '.jpeg', '.png', '.arw', '.nef', '.tiff', '.webp', '.bmp', '.cr2', '.orf', '.rw2', '.rwl', '.srw']:
-                    datetime_obj = self.get_exif_date(file_path)
-                elif extension in ['.mov', '.mp4']:
-                    datetime_obj = self.get_media_date(file_path)
-                else:
-                    datetime_obj = None
-
-                if datetime_obj is not None:
-                    new_name = self.get_formatted_date(datetime_obj, filename) + extension
-                    self.textbox_3.insert(tkinter.END, f"{filename} -> {new_name}\n")
-                    file_count += 1
-                    if file_count >= 50:
-                        break
+                    if extension in ['.jpg', '.jpeg', '.png', '.arw', '.nef', '.tiff', '.webp', '.bmp', '.cr2', '.orf', '.rw2', '.rwl', '.srw']:
+                        datetime_obj = self.get_exif_date(file_path)
+                    elif extension in ['.mov', '.mp4']:
+                        datetime_obj = self.get_media_date(file_path)
+                    else:
+                        datetime_obj = None
+                    if datetime_obj is not None:
+                        new_name = self.get_formatted_date(datetime_obj, filename) + extension
+                        app.textbox_3.insert(tkinter.END, f"{filename} -> {new_name}\n")
+                        file_count += 1
+                        if file_count >= 50:
+                            break
+        except Exception as e:
+                app.textbox_2.insert("0.0", f"Error: {str(e)}\n\n")
 
     def get_formatted_date(self, datetime_obj, filename, index=None):
         format_str = self.combobox_1.get()
